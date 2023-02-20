a4b9a03e3dceb73c9509f80c61b688f456e2bb0b
Maciek Sitkowski <msitkowski94@gmail.com>
2023-02-20 20:26:52 +0100

    feat 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import logo from './logo.svg'
 import './App.css'
 
@@ -11,12 +11,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => setCount(count => count + 1)}>
             count is: {count}
           </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
